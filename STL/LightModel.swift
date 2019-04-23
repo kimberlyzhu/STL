20492704c16215f0ce0090d989f1b4267a33899b
Kimberly Zhu <kimberly.zhu.2000@gmail.com>
2019-04-22 20:18:32 -0700

    firebase connection working for queries

---
+++ STL/LightModel.swift
@@ -0,0 +1,20 @@
+//
+//  LightModel.swift
+//  STL
+//
+//  Created by Kimberly Zhu on 4/22/19.
+//  Copyright © 2019 UCB-Team-Tech. All rights reserved.
+//
+
+import Foundation
+class LightModel {
+    
+    var light: Int?
+    var power: Double?
+    
+    init(light: Int?, power: Double?){
+        self.light = light
+        self.power = power
+    }
+    
+}
