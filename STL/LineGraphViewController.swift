67390fc5abcb41b6a1f43e992fb26fe2f6f2ace1
Kimberly Zhu <kimberly.zhu.2000@gmail.com>
2019-04-30 02:05:31 -0700

    line charats working. Implementing page segue + icons

--- STL/LineGraphViewController.swift
+++ STL/LineGraphViewController.swift
@@ -8,10 +8,32 @@
 
 import Foundation
 import UIKit
+import Charts
 
 class LineGraphViewController: UIViewController {
+    @IBOutlet weak var backButton: UIButton!
+    
+    @IBOutlet weak var lineChartView: LineChartView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setChartValues();
+    }
+    @IBAction func backButton(_ sender: Any) {
+       self.dismiss(animated: true)
+    }
+    func backAction() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    func setChartValues(_ count : Int = 20) {
+        let values = (0..<count).map { (i) -> ChartDataEntry in
+            let val = Double(arc4random_uniform(UInt32(count)) + 3)
+            return ChartDataEntry(x: Double(i), y: val)
+        }
+        
+        let set1 = LineChartDataSet(values: values, label: "DataSet 1")
+        let data = LineChartData(dataSet: set1)
+        
+        self.lineChartView.data = data
     }
 }
