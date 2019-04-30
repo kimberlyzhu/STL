74891899e808b4f50d52e9ee2f8502618f5f073c
Kimberly Zhu <kimberly.zhu.2000@gmail.com>
2019-04-29 18:30:17 -0700

    add line graph

---
+++ STL/LineGraphViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LineGraphController.swift
+//  STL
+//
+//  Created by Kimberly Zhu on 4/29/19.
+//  Copyright © 2019 UCB-Team-Tech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LineGraphViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
