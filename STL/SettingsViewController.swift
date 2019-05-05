9bfd91ac9b43da7e180db47bd900f0bf20b6be8f
Kimberly Zhu <kimberly.zhu.2000@gmail.com>
2019-05-05 16:33:39 -0700

    fixed images

---
+++ STL/SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.swift
+//  STL
+//
+//  Created by Kimberly Zhu on 5/5/19.
+//  Copyright © 2019 UCB-Team-Tech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+}
