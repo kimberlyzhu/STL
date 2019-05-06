c9501175d7f1bd8c9e0f414ea609aed18fea11f0
Kimberly Zhu <kimberly.zhu.2000@gmail.com>
2019-05-05 17:41:57 -0700

    add QR

---
+++ STL/AddProjectViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddProjectViewController.swift
+//  STL
+//
+//  Created by Kimberly Zhu on 5/5/19.
+//  Copyright © 2019 UCB-Team-Tech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddProjectViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+}
+
